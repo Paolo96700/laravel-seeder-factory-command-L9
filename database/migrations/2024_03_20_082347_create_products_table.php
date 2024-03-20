cdea47619819887172eb81a41819d73be3da3a5d
Paolo Falco <paolo.falcoapp@gmail.com>
2024-03-20 09:25:43 +0100

    add migration

---
+++ database/migrations/2024_03_20_082347_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->string('name');
+            $table->text('description');
+            $table->string('image');
+            $table->string('ean_code');
+            $table->string('price');
+            $table->string('evidence');
+            $table->timestamps();
+
+            // Aggiungi la chiave esterna
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
