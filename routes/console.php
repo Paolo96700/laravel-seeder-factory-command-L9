17f56a5cf7e835ce0fe5f9e411251237ea526dd6
Paolo Falco <paolo.falcoapp@gmail.com>
2024-03-21 17:36:01 +0100

    add command for update products

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
@@ -17,3 +18,17 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('products', function () {
+    $products = Product::all('id', 'name', 'price', 'evidence');
+    $this->table(['ID', 'Nome', 'Prezzo', 'Visibillità'], $products->toArray());
+})->purpose('questo comando ti permette di vedere i prodotti');
+
+Artisan::command('products:update-featured', function () {
+    $evidenceProducts = Product::where('evidence', 1)->get(['id', 'name', 'price', 'evidence']);
+    if(Product::where('evidence', 1)){
+        $this->table(['ID', 'Nome', 'Prezzo', 'Visibillità'], $evidenceProducts->toArray());
+    } else{
+        echo "non ci sono prodotti in evidenza";
+    }
+})->purpose('questo comando ti permette di vedere i prodotti in evidenza');
